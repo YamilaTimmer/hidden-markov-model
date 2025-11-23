bff7ba1b7b0469dc4861926ae225a53d0459e363
Yamila <yamilatimmer99@gmail.com>
2025-11-23 15:08:00 +0100

    Score methode aangepast voor forward algoritme, lijkt te werken voor korte sequentie van 3, maar lijkt nog verkeerde output te geven bij de sequenties van >30 (uitkomst verschilt met dat van hmmlearn)

--- hmmmodel.py
+++ hmmmodel.py
@@ -2,8 +2,7 @@ import math
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-from itertools import product
-from math import exp, log
+
 
 class HiddenMarkovModel:
     def __init__(self, startprob, transprob, emissionprob, states, emissions):
@@ -74,13 +73,38 @@ class HiddenMarkovModel:
 
 
     def score(self, emission_sequence, state_sequence=None):
+
+        # klein getal om 0 waarden mee te vervangen voor de log berekening
+        epsilon = 1e-10
+
         if state_sequence is None:
         # Implementatie forward-algoritme
-            return
+
+            E = len(emission_sequence)
+            S = len(self.states)
+            F = np.zeros((S, E))
+
+            # Bereken de kansen voor de start staat, gegeven de emissie
+            for i, state in enumerate(self.states):
+                F[i, 0] = self.startprob[state] * max(self.emissionprob[state][emission_sequence[0]], epsilon)
+
+            for t in range(1, E):
+                for i, curr_state in enumerate(self.states):
+                    total = 0.0
+
+                    for j, prev_state in enumerate(self.states):
+                        # Bereken de kansen op staat i gegeven de vorige staat en emissie
+                        total += F[j, t - 1] * self.transprob[prev_state][curr_state]
+                    # Tel alle kansen op staat i op
+                    F[i, t] = total * max(self.emissionprob[curr_state][emission_sequence[t]], epsilon)
+
+            total_probability = np.sum(F[:, -1])
+            log_probability = math.log(max(total_probability, epsilon))
+
+            return log_probability
 
         else:
-            # klein getal om 0 waarden mee te vervangen voor de log berekening
-            epsilon = 1e-10
+
 
             log_probability = (
                     math.log(self.startprob[state_sequence[0]]) +
